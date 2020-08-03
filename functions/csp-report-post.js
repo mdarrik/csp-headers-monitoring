eee7ccd02604ac0933d541123f2171f8c2ee36b1
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-08-02 19:05:35 -0700

    chore: adds honeycomb

--- functions/csp-report-post.js
+++ functions/csp-report-post.js
@@ -1,5 +1,21 @@
+const libHoney = require('libhoney')
+const honeycomb = new libHoney({
+    writeKey: process.env.HONEYCOMB_API_KEY,
+    dataset: process.env.HONEYCOMB_DATA_SET,
+})
 exports.handler = async (event, context) => {
+    const honeycombEvent = honeycomb.newEvent()
+    honeycombEvent.add({
+        functionName: context.functionName,
+        functionVersion: context.functionVersion,
+        requestId: context.awsRequestId,
+        userId: event.UserId,
+        userAction: event.UserAction,
+        body: event.body
+    })
     console.log(event.body)
+
+    honeycombEvent.send();
     return {
         headers: {
             "Access-Control-Allow-Origin": "https://www.darrik.dev",
